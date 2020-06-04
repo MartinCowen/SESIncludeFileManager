2496598b0f78796fd0ff82b98ba809b1f288014a
Martin Cowen <work@martincowen.me.uk>
2020-06-04 10:45:55 +0100

    Started designing form. Added a couple of settings.

--- SESIncludeFileManager/Form1.Designer.vb
+++ SESIncludeFileManager/Form1.Designer.vb
@@ -22,10 +22,154 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtSDKFolder = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtProjectFile = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lstPaths = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lstFiles = New System.Windows.Forms.ListBox()
+        Me.txtSearchFiles = New System.Windows.Forms.TextBox()
+        Me.btnReadProject = New System.Windows.Forms.Button()
+        Me.btnUpdateProject = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SDK Folder"
+        '
+        'txtSDKFolder
+        '
+        Me.txtSDKFolder.Location = New System.Drawing.Point(80, 10)
+        Me.txtSDKFolder.Name = "txtSDKFolder"
+        Me.txtSDKFolder.Size = New System.Drawing.Size(391, 20)
+        Me.txtSDKFolder.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "SES Project File"
+        '
+        'txtProjectFile
+        '
+        Me.txtProjectFile.Location = New System.Drawing.Point(107, 47)
+        Me.txtProjectFile.Name = "txtProjectFile"
+        Me.txtProjectFile.Size = New System.Drawing.Size(559, 20)
+        Me.txtProjectFile.TabIndex = 3
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(578, 73)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 4
+        Me.btnBrowse.Text = "Browse"
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Include Paths"
+        '
+        'lstPaths
+        '
+        Me.lstPaths.FormattingEnabled = True
+        Me.lstPaths.Location = New System.Drawing.Point(19, 117)
+        Me.lstPaths.Name = "lstPaths"
+        Me.lstPaths.Size = New System.Drawing.Size(223, 342)
+        Me.lstPaths.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(272, 95)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(66, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Include Files"
+        '
+        'lstFiles
+        '
+        Me.lstFiles.FormattingEnabled = True
+        Me.lstFiles.Location = New System.Drawing.Point(274, 137)
+        Me.lstFiles.Name = "lstFiles"
+        Me.lstFiles.Size = New System.Drawing.Size(238, 316)
+        Me.lstFiles.TabIndex = 8
+        '
+        'txtSearchFiles
+        '
+        Me.txtSearchFiles.Location = New System.Drawing.Point(275, 111)
+        Me.txtSearchFiles.Name = "txtSearchFiles"
+        Me.txtSearchFiles.Size = New System.Drawing.Size(237, 20)
+        Me.txtSearchFiles.TabIndex = 9
+        '
+        'btnReadProject
+        '
+        Me.btnReadProject.Location = New System.Drawing.Point(578, 137)
+        Me.btnReadProject.Name = "btnReadProject"
+        Me.btnReadProject.Size = New System.Drawing.Size(88, 25)
+        Me.btnReadProject.TabIndex = 10
+        Me.btnReadProject.Text = "Read Project"
+        Me.btnReadProject.UseVisualStyleBackColor = True
+        '
+        'btnUpdateProject
+        '
+        Me.btnUpdateProject.Location = New System.Drawing.Point(578, 187)
+        Me.btnUpdateProject.Name = "btnUpdateProject"
+        Me.btnUpdateProject.Size = New System.Drawing.Size(88, 25)
+        Me.btnUpdateProject.TabIndex = 11
+        Me.btnUpdateProject.Text = "Update Project"
+        Me.btnUpdateProject.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(693, 476)
+        Me.Controls.Add(Me.btnUpdateProject)
+        Me.Controls.Add(Me.btnReadProject)
+        Me.Controls.Add(Me.txtSearchFiles)
+        Me.Controls.Add(Me.lstFiles)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lstPaths)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtProjectFile)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtSDKFolder)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "SES Include File Manager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtSDKFolder As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtProjectFile As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lstPaths As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lstFiles As ListBox
+    Friend WithEvents txtSearchFiles As TextBox
+    Friend WithEvents btnReadProject As Button
+    Friend WithEvents btnUpdateProject As Button
 End Class
--- SESIncludeFileManager/My Project/Settings.Designer.vb
+++ SESIncludeFileManager/My Project/Settings.Designer.vb
@@ -13,57 +13,82 @@ Option Explicit On
 
 
 Namespace My
-
-    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(), _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0"), _
-     Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
+    
+    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0"),  _
+     Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
-
-        Private Shared defaultInstance As MySettings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New MySettings), MySettings)
-
+        
+        Private Shared defaultInstance As MySettings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New MySettings()),MySettings)
+        
 #Region "My.Settings Auto-Save Functionality"
 #If _MyType = "WindowsForms" Then
-        Private Shared addedHandler As Boolean
+    Private Shared addedHandler As Boolean
 
-        Private Shared addedHandlerLockObject As New Object
+    Private Shared addedHandlerLockObject As New Object
 
-        <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
-        Private Shared Sub AutoSaveSettings(ByVal sender As Global.System.Object, ByVal e As Global.System.EventArgs)
-            If My.Application.SaveMySettingsOnExit Then
-                My.Settings.Save()
-            End If
-        End Sub
+    <Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)> _
+    Private Shared Sub AutoSaveSettings(sender As Global.System.Object, e As Global.System.EventArgs)
+        If My.Application.SaveMySettingsOnExit Then
+            My.Settings.Save()
+        End If
+    End Sub
 #End If
 #End Region
-
+        
         Public Shared ReadOnly Property [Default]() As MySettings
             Get
-
+                
 #If _MyType = "WindowsForms" Then
-                   If Not addedHandler Then
-                        SyncLock addedHandlerLockObject
-                            If Not addedHandler Then
-                                AddHandler My.Application.Shutdown, AddressOf AutoSaveSettings
-                                addedHandler = True
-                            End If
-                        End SyncLock
-                    End If
+               If Not addedHandler Then
+                    SyncLock addedHandlerLockObject
+                        If Not addedHandler Then
+                            AddHandler My.Application.Shutdown, AddressOf AutoSaveSettings
+                            addedHandler = True
+                        End If
+                    End SyncLock
+                End If
 #End If
                 Return defaultInstance
             End Get
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("C:\Bluetooth\Nordic_Semi\nRF5_SDK_12.3.0_d7731ad\")>  _
+        Public Property SDKFolder() As String
+            Get
+                Return CType(Me("SDKFolder"),String)
+            End Get
+            Set
+                Me("SDKFolder") = value
+            End Set
+        End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("C:\Bluetooth\Nordic_Semi\nRF5_SDK_12.3.0_d7731ad\examples\ble_central\experimenta"& _ 
+            "l\ble_app_blinky_c\pca10028\s130\ses\ble_app_blinky_c_pca10028_s130.emProject")>  _
+        Public Property SESProjectFile() As String
+            Get
+                Return CType(Me("SESProjectFile"),String)
+            End Get
+            Set
+                Me("SESProjectFile") = value
+            End Set
         End Property
     End Class
 End Namespace
 
 Namespace My
-
-    <Global.Microsoft.VisualBasic.HideModuleNameAttribute(), _
-     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(), _
-     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()> _
+    
+    <Global.Microsoft.VisualBasic.HideModuleNameAttribute(),  _
+     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
-
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
+        
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
         Friend ReadOnly Property Settings() As Global.SESIncludeFileManager.My.MySettings
             Get
                 Return Global.SESIncludeFileManager.My.MySettings.Default
